2b29581dabfb758ed6347639e71e7a1fc5e388eb
Martin Shay <martin.shay@deepstreamhub.com>
2017-06-02 10:12:44 +0200

    refactor: change to arrow functions, like events script.

--- presence.js
+++ presence.js
@@ -6,36 +6,36 @@ const clientC = deepstream('localhost:6020')
 const clientD = deepstream('localhost:6020')
 const clientE = deepstream('localhost:6020')
 
-setTimeout(function () {
+setTimeout(() => {
   clientA.presence.subscribe((username, isLoggedIn) => {
     console.log(username, isLoggedIn)
   })
 }, 10)
 
-setTimeout(function () {
+setTimeout(() => {
   clientB.login({username: 'Rick'})
 }, 40)
 
-setTimeout(function () {
+setTimeout(() => {
   clientC.login({username: 'Summer', password: 'pwd'})
 }, 70)
 
-setTimeout(function () {
+setTimeout(() => {
   clientD.login()
 }, 100)
 
-setTimeout(function () {
+setTimeout(() => {
   clientA.presence.getAll((clients) => {
     console.log('connected clients:', clients)
   })
 }, 130)
 
 // docs mention a callback being not-optional.  i havn't included one here.  should i be triggering an error?
-setTimeout(function () {
+setTimeout(() => {
   clientA.presence.unsubscribe()
 }, 150)
 
-setTimeout(function () {
+setTimeout(() => {
   clientE.login({username: 'Beth', password: 'pwd'})
 }, 170)
 
